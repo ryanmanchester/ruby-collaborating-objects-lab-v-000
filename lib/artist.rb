5afaeb9edb9a97f01ec7dc2087a4fcc1f1d99bbb
Ryan Manchester <ryanm17@gmail.com>
2018-01-15 01:54:34 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,7 +25,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-     binding.pry
+    # binding.pry
     artist = self.all.detect{|n| n.name == name}
     if artist != nil
         artist

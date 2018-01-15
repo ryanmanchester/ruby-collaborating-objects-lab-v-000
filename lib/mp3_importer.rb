9fd55f1a0e0a946ede4f01b88764f40a72d9666e
Ryan Manchester <ryanm17@gmail.com>
2018-01-15 01:04:56 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -12,12 +12,12 @@ class MP3Importer
   #binding.pry
  end
 
- def import
-   files.each do |file|
-     #Song.new_by_filename(file)
-     #binding.pry
-   end
- end
+ # def import
+ #   files.each do |file|
+ #     Song.new_by_filename(file)
+ #
+ #   end
+ # end
 
 
 

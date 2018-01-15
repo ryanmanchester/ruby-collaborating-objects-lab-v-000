7c37f60ec1e3635f12155bdc28bc398f747e3f1c
Ryan Manchester <ryanm17@gmail.com>
2018-01-15 01:59:11 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -30,8 +30,10 @@ class Artist
     if artist != nil
         artist
     else
-      Artist.new(name)
+    artist =  Artist.new(name)
+    artist.save
     end
+    artist
   end
 
   def print_songs

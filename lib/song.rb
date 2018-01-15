d43bbea0816b875abec800446b5ee49642d31d67
Ryan Manchester <ryanm17@gmail.com>
2018-01-15 00:50:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,13 +10,13 @@ def name
   @name
 end
 
-def self.new_by_filename(filename)
-  new_file = filename.split(" - ")
-  new_instance = Song.new(new_file[1])
+# def self.new_by_filename(filename)
+#   new_file = filename.split(" - ")
+#   new_instance = Song.new(new_file[1])
   #new_instance.artist_name=(name)
   #song
   #binding.pry
-end
+#end
 
 def artist_name=(name)
   Artist.find_or_create_by_name(name)

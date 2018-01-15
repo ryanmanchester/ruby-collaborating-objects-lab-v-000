2c46ffbb97235e37cff52e078f3650546840ba26
Ryan Manchester <ryanm17@gmail.com>
2018-01-15 00:38:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,8 +19,8 @@ def self.new_by_filename(filename)
 end
 
 def artist_name=(name)
-  Artist.find_or_create_by_name(name)
-  self.artist.add_song(song)
+  Artist.find_or_create_by_name(name).artist.add_song(song)
+
   #binding.pry
 end
 
